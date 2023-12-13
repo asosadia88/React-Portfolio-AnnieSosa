990d09925c1173a1c7da2e23a76fc14566aef282
asosadia88 <annielys.sosa@gmail.com>
2023-12-13 01:59:14 -0500

    tranferred Project 2 files

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,20 @@
+export default function About() {
+  return (
+    <section className="left-side" id="about">
+
+    <h1 className="title">About Me</h1>
+    <section className="section-container">
+        <p> My name is Annielys Sosa. I was born in Habana, Cuba and emigrated to sunny Miami with my parents at the
+            age of 7. </p>
+
+        <p>I earned my Bachelor in Microbiology and Cell Science from the University of Florida and Master in
+            Finance from the University of Miami. </p>
+        
+
+        <p> I am an ERP Systems Analyst at Miami Dade College and recent graduate of the UM Full Stack Wed Development BootCamp. 
+            You may contact me, browse my previous work, or listen to some of my favorite tunes below.
+        </p>
+    </section>
+</section>
+  );
+}
---
+++ src/components/pages/Projects.jsx
@@ -0,0 +1,28 @@
+export default function Projects() {
+  return (
+    <section className="left-side" id="work">
+
+        <h1 className="title">Work</h1>
+        <section className="section-container">
+            <section id="image1">
+                <a href="https://asosadia88.github.io/Horiseon-Challenge-3/"><img src="images/screen_shot.PNG"
+                        alt="screenshot" id="hw1"/></a>
+                <h3>Previous Work</h3>
+            </section>
+            <section className="card">
+                <a href="https://dsternthal.github.io/Project1-Travel/"><img
+                        src="images/SoFloScreenshot.png" alt="Project1Screenshot"/></a>
+                <h3>Project 1: SoFlo Food</h3>
+            </section>
+
+            <section className="card">
+                <a href="https://pawgress-263620beafdd.herokuapp.com/"><img
+                        src="images/Project2Screenshot.png" alt="Project2Screenshot"/></a>
+                <h3>Project 2: Pawgress.com</h3>
+            </section>
+
+
+        </section>
+    </section>
+  );
+}
